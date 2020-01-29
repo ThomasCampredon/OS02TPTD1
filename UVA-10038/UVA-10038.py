9ad33c3cd9e6b61ef73e67e99c8c82657c2c3171
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2020-01-29 15:42:32 +0100

    UVA-10038

--- UVA-10038/UVA-10038.py
+++ UVA-10038/UVA-10038.py
@@ -16,14 +16,13 @@ while True:
             listeNombre.append(i)
         for j in range (1, nbInt):
             listeDifference.append(abs(tab[j]-tab[j+1]))
-        print (listeNombre)
         for k in range(0, len(listeDifference)):
             if not listeDifference[k] in listeNombre:
-                if not listeNombre[k] in listeDifference:
-                    estJolly=False
+                 estJolly=False
+        for l in range(0, len(listeNombre)):                
+            if not listeNombre[l] in listeDifference:
+                estJolly=False
         if estJolly:
             print("Jolly")
         else:
             print("Not jolly")
-
-        
